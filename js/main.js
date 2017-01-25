51c6999caa76f2730038ea4fd9cf67efa9c21aed
n8659150 <jackli900421@hotmail.com>
2017-01-25 17:08:38 +0800

    divide ranking list data into seperate file and fetch it using ajax.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 "use strict";
 window.onload = (function(){
-alert("ready");
 // slider.js
 // 设定初始化的轮播图背景
 let oSlider = document.getElementById("slider");
@@ -69,34 +68,19 @@ let oRankingList = document.getElementById("ranking-list");
 let aRankingItems = oRankingList.getElementsByClassName("ranking-content");
 let oHotListTab = document.getElementById("hot-list-tab");
 let oNewListTab = document.getElementById("new-list-tab");
-let hotRankingList = [
-	{rank:1,name:"欢乐斗地主",count:2263760},
-	{rank:2,name:"夺宝斗地主",count:1129088},
-	{rank:3,name:"蛇蛇争霸",count:848812},
-	{rank:4,name:"欢乐麻将全集",count:624418},
-	{rank:5,name:"犀利蛇",count:490240},
-	{rank:6,name:"海底大作战",count:407488},
-	{rank:7,name:"星球大乱斗",count:381616},
-	{rank:8,name:"捕鱼达人3D",count:299232},
-	{rank:9,name:"街篮高手",count:263808},
-	{rank:10,name:"开心消消乐",count:248176},
-	{rank:11,name:"梦幻练舞",count:245712},
-	{rank:12,name:"王者荣耀(电脑版)",count:238800}
-]
-let newGameRankingList = [
-	{rank:1,name:"欢乐斗地主",count:12263760},
-	{rank:2,name:"夺宝斗地主",count:11129088},
-	{rank:3,name:"蛇蛇争霸",count:1848812},
-	{rank:4,name:"欢乐麻将全集",count:624418},
-	{rank:5,name:"犀利蛇",count:490240},
-	{rank:6,name:"海底大作战",count:407488},
-	{rank:7,name:"星球大乱斗",count:381616},
-	{rank:8,name:"捕鱼达人3D",count:299232},
-	{rank:9,name:"街篮高手",count:263808},
-	{rank:10,name:"开心消消乐",count:248176},
-	{rank:11,name:"梦幻练舞",count:245712},
-	{rank:12,name:"王者荣耀(电脑版)",count:238800}
-]
+
+function fetchRankingList(sourcename){
+	let xmlhttp = new XMLHttpRequest();
+	let sourceUrl = "js/"+sourcename+".json";
+	xmlhttp.open("GET",sourceUrl,false);
+	xmlhttp.send();
+	let rankingData = JSON.parse(xmlhttp.responseText);
+	return rankingData;
+}
+
+let hotRankingList = fetchRankingList("hotRankingListData");
+let newGameRankingList = fetchRankingList("newRankingListData");
+
 function rankingListLoader(list){
 	for(let i=0;i<aRankingItems.length;i++){
 		for(let j=0;j<list.length;j++){
