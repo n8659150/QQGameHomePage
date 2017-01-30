3f5a449fe673cd95d5cccabdbebad7baa8a69373
n8659150 <jackli900421@hotmail.com>
2017-01-30 22:22:28 +0800

    implementing JQuery,simplify previous code.

--- js/main.js
+++ js/main.js
@@ -2,45 +2,48 @@
 $(document).ready(function(){
 // slider.js
 // 设定初始化的轮播图背景
-let oSlider = document.getElementById("slider");
-oSlider.style.background = "url(images/slides/slider-1.jpg) no-repeat";
+$("#slider").css({"background":"url(images/slides/slider-1.jpg) no-repeat"});
 var sliderIndex = 1;
-// 初始化右侧thumbnail小图片
-let oSliderThumb = document.getElementById("slider-thumbnail");
 //给轮播图右侧thumbnail设置悬停高亮及移出取消
-let aSliderThumbItems = oSliderThumb.getElementsByTagName("li");
+let aSliderThumbItems = $("#slider-thumbnail").find("li");
 
 for (let i = 0; i<aSliderThumbItems.length;i++) {
-	let oDiv = aSliderThumbItems[i].getElementsByTagName("div")[0];
-	oDiv.addEventListener("mouseover", function(){
+	let oDiv = $("#slider-thumbnail").find("li").eq(i).find("div:first");
+	oDiv.mouseover(function(){
 		setActive(this);
 	});
-	oDiv.addEventListener("mouseout", function(){
+	oDiv.mouseout(function(){
 		cancelActive(this);
 	});
 }
 
 function setActive(element){
-  element.getElementsByTagName("img")[0].setAttribute("class", "active");
-  element.getElementsByClassName("title")[0].setAttribute("class","title active");
+	$(element).find("img:first").addClass("active");
+	$(element).find(".title:first").addClass("title active");
+  // element.getElementsByTagName("img")[0].setAttribute("class", "active");
+  // element.getElementsByClassName("title")[0].setAttribute("class","title active");
 }
 
 function cancelActive(element) {
-	element.getElementsByTagName("img")[0].setAttribute("class","");
-	element.getElementsByClassName("title")[0].setAttribute("class","title");
+	$(element).find("img:first").removeClass("active");
+	$(element).find(".title:first").removeClass("active");
+	// element.getElementsByTagName("img")[0].setAttribute("class","");
+	// element.getElementsByClassName("title")[0].setAttribute("class","title");
 }
 
 function sliderChange(){
 	//重置所有高亮样式
-	let aSliderThumbItems = oSliderThumb.getElementsByTagName("li");
+	//let aSliderThumbItems = oSliderThumb.getElementsByTagName("li");
+	let aSliderThumbItems = $("#slider-thumbnail").find("li");
 	for (let j = 0; j < aSliderThumbItems.length; j++) {
-		cancelActive(aSliderThumbItems[j].getElementsByTagName("div")[0]);
+		cancelActive(aSliderThumbItems.eq(j).find("div:first"));
 	}
 	//根据当前轮播图背景设置高亮
-	let oLi = oSliderThumb.getElementsByTagName("li")[sliderIndex-1];
-	let oDiv = oLi.getElementsByTagName("div")[0];
+	let oLi = $("#slider-thumbnail").find("li").eq(sliderIndex-1);
+	let oDiv = oLi.find("div:first");
 	setActive(oDiv);
-	oSlider.style.background = "url(images/slides/slider-"+ (sliderIndex) +".jpg) no-repeat";
+	$("#slider").css({"background":"url(images/slides/slider-"+ (sliderIndex) +".jpg) no-repeat"});
+	//oSlider.style.background = "url(images/slides/slider-"+ (sliderIndex) +".jpg) no-repeat";
 	sliderIndex>
 	3?sliderIndex=1:sliderIndex++;
 };
@@ -49,18 +52,23 @@ setInterval(sliderChange, 4000);
 // gameTabPicLoader.js
 
 // 热门游戏
-let oHotGames = document.getElementById("hot-games");
-let aHotGameTabs = oHotGames.getElementsByClassName("game-tab");
+//let oHotGames = document.getElementById("hot-games");
+//let aHotGameTabs = oHotGames.getElementsByClassName("game-tab");
+let aHotGameTabs = $("#hot-games").find(".game-tab");
 for (let i = 0; i < aHotGameTabs.length;i++) {
-	aHotGameTabs[i].style.background = "url(images/hot-game-pics/"+ (i) +".jpg) no-repeat";
+	aHotGameTabs.eq(i).css({"background":"url(images/hot-game-pics/"+ (i) +".jpg) no-repeat"});
 }
 
 //最新游戏
-let oLatestGames = document.getElementById("latest-games");
+// let oLatestGames = document.getElementById("latest-games");
 
-let aLatestGameTabs = oLatestGames.getElementsByClassName("game-tab");
+// let aLatestGameTabs = oLatestGames.getElementsByClassName("game-tab");
+// for (let i = 0; i < aLatestGameTabs.length;i++) {
+// 	aLatestGameTabs[i].style.background = "url(images/latest-game-pics/"+ (i) +".jpg) no-repeat";
+// }
+let aLatestGameTabs = $("#latest-games").find(".game-tab");
 for (let i = 0; i < aLatestGameTabs.length;i++) {
-	aLatestGameTabs[i].style.background = "url(images/latest-game-pics/"+ (i) +".jpg) no-repeat";
+	aLatestGameTabs.eq(i).css({"background":"url(images/latest-game-pics/"+ (i) +".jpg) no-repeat"});
 }
 //rankingListData.js
 let oRankingList = document.getElementById("ranking-list");
@@ -79,17 +87,6 @@ function fetchRankingList(sourcename){
 let hotRankingList = fetchRankingList("hotRankingListData");
 let newGameRankingList = fetchRankingList("newRankingListData");
 
-// function rankingListLoader(list){
-// 	for(let i=0;i<aRankingItems.length;i++){
-// 		for(let j=0;j<list.length;j++){
-// 			let rankingItem = aRankingItems[j];
-// 			let listData = list[j];
-// 			rankingItem.getElementsByClassName("ranking-number")[0].innerHTML=listData["rank"];
-// 			rankingItem.getElementsByClassName("ranking-name")[0].innerHTML=listData["name"];
-// 			rankingItem.getElementsByClassName("ranking-count")[0].innerHTML=listData["count"];
-// 		}
-// 	}
-// }
 function rankingListLoader(list){
 	let ele = $("#ranking-list").find(".ranking-content");
 	for (let i=0;i<list.length;i++){
@@ -98,10 +95,11 @@ function rankingListLoader(list){
 		ele.find(".ranking-count").eq(i).text(list[i]["count"]);
 	}
 }
-oHotListTab.addEventListener("mouseover", function(){
+
+$("#hot-list-tab").mouseover(function(){
 		rankingListLoader(hotRankingList);
 	});
-oNewListTab.addEventListener("mouseover", function(){
+$("#new-list-tab").mouseover(function(){
 		rankingListLoader(newGameRankingList);
 	});
 rankingListLoader(hotRankingList);
@@ -117,24 +115,18 @@ function fetchAppGames(sourcename){
 function appGameLoader(sourcename,appGames){
 //sourcename所传入的参数对应fetchAppGames中的sourcename文件名，和本地json文件名相同。
 let appGameData = fetchAppGames(sourcename);
-// appGames所传入的参数和对应HTML区域所指定的id相同
-let oCardApps = document.getElementById(appGames);
-let aCardAppIcons = oCardApps.getElementsByClassName("app-icon");
-let oCardGameCount = document.getElementById(appGames+"-count");
-oCardGameCount.innerHTML = "("+appGameData.length+"款)";
-	for (let i=0;i<aCardAppIcons.length;i++){
-		for (let j=0;j<appGameData.length;j++){
-			let cardAppIcon = aCardAppIcons[j];
-			cardAppIcon.style.background = "url(images/app-game-icons/"+appGameData[j]["id"]+".png) no-repeat";
-			cardAppIcon.innerHTML = appGameData[j]["name"];
-			cardAppIcon.style.backgroundSize = "100%";
-		}
+$(appGames+"-count").text("("+appGameData.length+"款)");
+	for (let i=0;i<appGameData.length;i++){
+		let cardAppIcon = $(appGames).find(".app-icon").eq(i); 
+		cardAppIcon.css({"background":"url(images/app-game-icons/"+appGameData[i]["id"]+".png) no-repeat"});
+		cardAppIcon.css({"background-size":"100%"});
+		cardAppIcon.text(appGameData[i]["name"]);
 	}
 }
 // 棋牌游戏
-appGameLoader("appGameLoader","card-apps");
+appGameLoader("appGameLoader","#card-apps");
 // 网页游戏
-appGameLoader("appGameLoader","web-apps")
+appGameLoader("appGameLoader","#web-apps")
 
 // server timetable
 function fetchNewServerData(){
@@ -143,24 +135,18 @@ function fetchNewServerData(){
 	httpReq.send();
 	return JSON.parse(httpReq.responseText); 
 }
-let aNewServerData = fetchNewServerData();
 fetchNewServerData();
-let oNewServerTable = document.getElementById("new-server-timetable");
-let aNewServerContents = oNewServerTable.getElementsByClassName("new-server-content");
-for (let i = 0; i < aNewServerContents.length; i++){
-	for (let j = 0; j < aNewServerData.length;j++){
+let aNewServerData = fetchNewServerData();
+let aNewServerContents = $("#new-server-timetable").find(".new-server-content");
+	for (let i = 0; i < aNewServerContents.length; i++){
 		// 防止数组越界，数据（aNewServerData）可能会多于屏幕上显示的数目（aNewServerData）
-		if(aNewServerContents[j] == undefined) {
+		if(!(aNewServerContents.eq(i))){
 			return;
 		}
-		let serverGameIcon = aNewServerContents[j].getElementsByClassName("server-game-icon")[0];
-		let serverGameName = aNewServerContents[j].getElementsByClassName("server-game-name")[0];
-		serverGameIcon.style.background = "url(images/server-timetable-pics/"+(j)+".png) no-repeat";
-		serverGameIcon.style.backgroundSize = "cover";
-		serverGameName.innerHTML = aNewServerData[j]["name"];
-		
+		let serverGameIcon = aNewServerContents.eq(i).find(".server-game-icon:first");
+		let serverGameName = aNewServerContents.eq(i).find(".server-game-name:first");
+		serverGameIcon.css({"background":"url(images/server-timetable-pics/"+(i)+".png) no-repeat"});
+		serverGameIcon.css({"background-size":"100%"});
+		serverGameName.text(aNewServerData[i]["name"]);
 	}
-	
-}
-
 });
